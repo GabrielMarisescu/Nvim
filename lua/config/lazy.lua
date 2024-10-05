4e43db2ecd8050bc52d548df8696125ab1b8a72c
GabrielMarisescu <gabriel.lavoro10@gmail.com>
2024-10-05 22:21:49 +0200

    added miniindentscope

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
 
     { import = "plugins" },
   },
